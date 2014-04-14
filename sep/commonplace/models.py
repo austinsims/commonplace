fb2a7c55a67385b0390348451468933a0a497157
Austin Sims <sims4@purdue.edu>
2014-04-14 17:56:58 -0400

    changed categories not required; this fixes a bug that prevents you from creating the first item from a blank database.

--- sep/commonplace/models.py
+++ sep/commonplace/models.py
@@ -9,13 +9,18 @@ class Category(models.Model):
     def __unicode__(self):
         return self.name
 
+class Folder(models.Model):
+    name = models.CharField(max_length=200)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
 class Item(models.Model):
     creation_date = models.DateTimeField('date created', auto_now_add=True)
     title = models.CharField(max_length=400)
     description = models.CharField(max_length=2000)
     url = models.URLField(max_length=2000, unique=True) # don't let two users submit same item
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    categories = models.ManyToManyField(Category)
+    categories = models.ManyToManyField(Category, blank=True)
+#    folders = models.ManyToManyField(Folder, blank=True)
     def __unicode__(self):
         return self.title
 
